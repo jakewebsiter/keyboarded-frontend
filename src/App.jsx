d077db61808114ece668168b51435cd09de98057
Jake Harris <jakeharris30@outlook.com>
2023-06-01 12:06:51 +0100

    tailwind configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/navbar";
-import "./css/app.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar';
+import './index.css';
 
 function App() {
   return (
     <Router>
       <Navbar>
         <Routes>
-          <Route path="/" element={<p>Hello!</p>} />
-          <Route path="/bob" element={<p>Bob</p>} />
-          <Route path="/yeah" element={<p>Yeah</p>} />
+          <Route path='/' element={<p className='text-3xl'>Hello!</p>} />
+          <Route path='/bob' element={<p className='text-3xl'>Bob</p>} />
+          <Route path='/yeah' element={<p className='text-3xl'>Yeah</p>} />
         </Routes>
       </Navbar>
     </Router>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,19 @@
-import { NavLink } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
 
 export default function Navbar({ children }) {
   return (
     <>
-      <nav>
-        <NavLink to="/">Home</NavLink>
-        <div className="main-links">
-          <NavLink to="/bob">Bob</NavLink>
-          <NavLink to="/yeah">Yeah</NavLink>
+      <nav className='fixed top-0 left-0 w-screen flex gap-32 text-white bg-primary text-2xl'>
+        <NavLink to='/' className='nav-link ml-4'>
+          Home
+        </NavLink>
+        <div className='flex gap-16'>
+          <NavLink to='/bob' className='nav-link'>
+            Bob
+          </NavLink>
+          <NavLink to='/yeah' className='nav-link'>
+            Yeah
+          </NavLink>
         </div>
       </nav>
       {children}
