80e81048c1fc01f06aa134f4a93f0a11b7d77bbb
Jake Harris <jakeharris30@outlook.com>
2023-06-01 12:08:04 +0100

    tailwind + postcss config files added

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  mode: 'jit',
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0802b3',
+        primaryhover: '#050094',
+      },
+    },
+  },
+  plugins: [],
+};
